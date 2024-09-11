577567deae90bc469f80ee37cb1bad35310c3876
Linexl <58422273+Linexl@users.noreply.github.com>
2024-09-11 20:40:18 +0800

    Update www.69shuba.pro.js
    69shu.me

--- sources/www.69shuba.pro.js
+++ sources/www.69shuba.pro.js
@@ -4,7 +4,7 @@
 * @returns {[{name, author, cover, detail}]}
 */
 const search = (key) => {
-let response = POST("https://www.69shuba.pro/modules/article/search.php", {
+let response = POST("https://69shu.me/modules/article/search.php", {
 data: `searchkey=${ENCODE(key,"gbk")}&submit=Search`
 })
 let array = []
@@ -72,7 +72,7 @@ return $(".txtnav").remove("div,h1")
 }
 
 var bookSource = JSON.stringify({
-name: "69书吧-shuba.pro",
-url: "www.69shuba.pro",
+name: "69书吧-69shu.me",
+url: "69shu.me",
 version: 100
 })
