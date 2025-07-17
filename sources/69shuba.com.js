2a73a7cb96c31a390ab12a1d47b74d7793a01642
肖林 <xiaolinflyme@163.com>
2025-07-17 13:52:38 +0800

    dz2

--- sources/69shuba.com.js
+++ sources/69shuba.com.js
@@ -73,13 +73,13 @@ return JSON.stringify(array.reverse())
 */
 const chapter = (url) => {
 let response = GET(url).replace("(本章完)","")
+return url
 let $ = HTML.parse(response)
-return $
 return $(".txtnav").remove("div,h1")
 }
 
 var bookSource = JSON.stringify({
-name: "69书吧-69shuba.com-谍战生存指南1",
+name: "69书吧-69shuba.com-谍战生存指南2",
 url: "69shuba.com",
 version: 100
 })
